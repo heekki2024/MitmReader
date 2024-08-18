3201760aae5f894b7be3ddbe4d12f9adf626b59b
heekki2024 <heekki2024@gmail.com>
2024-08-18 19:38:37 +0900

    4

--- excel_IO.py
+++ excel_IO.py
@@ -1,9 +1,12 @@
 import openpyxl
 import re
+import os
+from openpyxl.utils.exceptions import IllegalCharacterError
 
+MAX_HOSTNAME_LENGTH = 31
 
 def excel_trackerList_input():
-    trackerList_path = r"C:\Users\xten\Desktop\trackList.xlsx"
+    trackerList_path = r"C:\Users\kfri1\Desktop\trackList.xlsx"
     wb = openpyxl.load_workbook(trackerList_path)
     ws = wb['Sheet1']
     if 'Sheet1' in wb.sheetnames:
@@ -14,33 +17,23 @@ def excel_trackerList_input():
     return trackerList
 
 def excel_prsnlList_input():
-    prsnlList_path = r"C:\Users\xten\Desktop\trackList.xlsx"
+    prsnlList_path = r"C:\Users\kfri1\Desktop\PersonalInfoList.xlsx"
     wb = openpyxl.load_workbook(prsnlList_path)
     ws = wb['Sheet1']
     if 'Sheet1' in wb.sheetnames:
         last_row = ws.max_row
-        print(last_row)
         prsnlList = []
         for i in range(1, last_row + 1, 1):
             prsnlList.append(ws[f'A{i}'].value)        
     return prsnlList
 
-count = 0
 
-# def match_trackerList(trackerList, host):
-#     for pattern in trackerList:
-#         # trackerList에 있는 항목이 host와 부분적으로 일치하는지 확인
-#         if re.search(pattern, host, re.IGNORECASE):
-#             global count
-#             count+=1
-#             print(count)
-#             return True,count
-#     return False
+
 
 def match_trackerList(trackerList, host):
     for pattern in trackerList:
         # trackerList의 각 항목을 단어로 검색하기 위해 단어 경계(\b)를 추가
-        word_pattern = rf'\b{re.escape(pattern)}\b'
+        word_pattern = rf'{re.escape(pattern)}\b'
         if re.search(word_pattern, host, re.IGNORECASE):
             global count
             count+=1
@@ -48,16 +41,92 @@ def match_trackerList(trackerList, host):
             return True
     return False
 
-# def match_trackerList(trackerList, host):
 
-#         for i in range(len(trackerList)):
-#             if host == trackerList[i]:
-#                 global count
-#                 count+=1
-#                 print(count)
-#                 return True
-#         return False  
+def match_prsnlList(prsnlList, kv):
 
+    if isinstance(kv, list):
+        for k, v in kv:
+            for pattern in prsnlList:
+                if pattern is None:  # None 무시
+                    continue
+                word_pattern = rf'{re.escape(str(pattern))}\b'  # 패턴을 문자열로 변환
+                if re.search(word_pattern, k, re.IGNORECASE) or re.search(word_pattern, v, re.IGNORECASE):
 
-# def match_prsnlList():
+                    return True
+
+    elif isinstance(kv, tuple) and len(kv) == 2:
+        k, v = kv
+        for pattern in prsnlList:
+            if pattern is None:  # None 무시
+                continue
+            word_pattern = rf'{re.escape(str(pattern))}\b'  # 패턴을 문자열로 변환
+            if re.search(word_pattern, k, re.IGNORECASE) or re.search(word_pattern, v, re.IGNORECASE):
+
+                return True
+                
+    elif isinstance(kv, str):
+        for pattern in prsnlList:
+            if pattern is None:  # None 무시
+                continue
+            word_pattern = rf'{re.escape(str(pattern))}\b'  # 패턴을 문자열로 변환
+            if re.search(word_pattern, kv, re.IGNORECASE):
+
+                return True
+                
+    return False
+
+# 엑셀 파일 경로 설정
+excel_file_path = r"C:\Users\kfri1\Desktop\231output.xlsx"
+
+def clean_host_name(host):
+    """
+    호스트 이름을 Excel 시트 이름으로 사용할 수 있도록 수정합니다.
+    길이를 31자로 제한하고 특수 문자를 제거합니다.
+    """
+    # 특수 문자 제거
+    clean_host = re.sub(r'[\/:*?"<>|]', '', host)
+    
+    # 길이 제한
+    if len(clean_host) > MAX_HOSTNAME_LENGTH:
+        clean_host = clean_host[:MAX_HOSTNAME_LENGTH]
+    
+    return clean_host
+
+def clean_string(value):
+    # 허용되지 않는 제어 문자를 제거
+    return re.sub(r'[\x00-\x1F\x7F]', '', value)
+
+def write_to_excel(host, data):
+    clean_host = clean_host_name(host)
+    if os.path.exists(excel_file_path):
+        wb = openpyxl.load_workbook(excel_file_path)
+    else:
+        wb = openpyxl.Workbook()
+
+        # 기본으로 생성되는 'Sheet' 시트를 제거
+        default_sheet = wb.active
+        wb.remove(default_sheet)
+
+    if clean_host in wb.sheetnames:
+        ws = wb[clean_host]
+    else:
+        ws = wb.create_sheet(title=clean_host)
+    # 현재 마지막 열을 찾기
+    last_column = ws.max_column
+    if last_column == 1 and ws.cell(row=1, column=1).value is None:
+        last_column = 0
+
+    # 새로운 열에서 데이터 입력 시작
+    start_column = last_column + 1
+    for i, value in enumerate(data, start=1):
+        try:
+            cleaned_value = clean_string(value)
+            ws.cell(row=i, column=start_column, value=cleaned_value)
+        except IllegalCharacterError as e:
+            print(f"Illegal character in value: {value}. Error: {e}") 
+
+    try:
+        wb.save(excel_file_path)
+    except Exception as e:
+        print(f"Error saving Excel file: {e}")
 
--- read_mitm_dump.py
+++ read_mitm_dump.py
@@ -17,6 +17,7 @@ import os
 # Enable ANSI escape sequences in Windows PowerShell
 os.system("")
 
+match = False
 
 def validate_and_read_file():
     if len(sys.argv) != 2:
@@ -45,7 +46,7 @@ def process_request_tracker(f, trackerList):
         if excel_IO.match_trackerList(trackerList, host):
             print(method, host)
             if method == 'GET':
-                print("\033[95m" + "GET@" + "\033[0m")
+                print("\033[95m" + "GET" + "\033[0m")
 
             print(f"path: {request.path}")
             
@@ -69,7 +70,7 @@ def process_request_tracker(f, trackerList):
             if len(headers) > 0:
                 print("headers: [")
                 for k, v in headers:
-                    print(f"\t{k}={v}")
+                    print(f"\t{k}: {v}")
                     if k.casefold() == "Content-Type".casefold():
                         vv = v.split(";")
                         contentType = vv[0]
@@ -93,8 +94,9 @@ def process_request_tracker(f, trackerList):
         else:
             print("매칭되는 호스트 없음")
 
-
-def process_request_personInfo(f, prsnlList):
+count = 0
+def process_request_personInfo(f, prsnlList, package_name):
+    global match
     #print(f)
     #isinstance(인스턴스, 데이터나 클래스 타입)
     #숫자 33이 int타입인지 확인이 필요하다면 result = isinstance(33, int)
@@ -105,79 +107,232 @@ def process_request_personInfo(f, prsnlList):
 
         method = request.method
         host = request.host
+        
+        word_pattern = rf'{excel_IO.re.escape(package_name)}\b'
 
-        print(method, host)
-        print(f"path: {request.path}")
+        if excel_IO.re.search(word_pattern, host, excel_IO.re.IGNORECASE):
+            return
+        # print(method, host)
+        # print(f"path: {request.path}")
         
         queries = request.query.items()
         if len(queries) > 0:
-            print("queries: [")
+            # print("queries: [")
             for k, v in queries:
-                print(f"\t{k}={v}")
-            print("]")
-        else:
-            print("queries: []")
+                if excel_IO.match_prsnlList(prsnlList, (k, v)):
+                    match = True
+            #     print(f"\t{k}={v}")
+            # print("]")
 
-        print("\n----------------------\n")
-        #------------------------------------
 
         contentType = ""
         contentLength = 0
 
         headers = request.headers.items()
         if len(headers) > 0:
-            print("headers: [")
+            # print("headers: [")
             for k, v in headers:
-                print(f"\t{k}={v}")
+                # print(f"\t{k}: {v}")
                 if k.casefold() == "Content-Type".casefold():
                     vv = v.split(";")
                     contentType = vv[0]
                 elif k.casefold() == "Content-Length".casefold():
                     contentLength = int(v)
-            print("]")
-        else:
-            print("headers: []")
 
+                if excel_IO.match_prsnlList(prsnlList, (k, v)):
+                    match = True
 
-        print("\n----------------------\n")
-        #------------------------------------
-        
+            # print("]")
+
+       
         if method == "POST":
-            process_post_request(request, contentType, contentLength)
-        else:
-            print("NONE")
+            if contentLength > 0:
+                text = request.get_text(False)
+                # print("Received JSON text:", text)  # 디버그용으로 추가
+                try:
+                    match contentType:
+                        case "application/x-www-form-urlencoded":
+                            data = parse_qs(text)
+                            if excel_IO.match_prsnlList(prsnlList, data.items()):
+                                match = True
+                        case "application/json":
+                            data = json.loads(text)
+                            if excel_IO.match_prsnlList(prsnlList, json.dumps(data, indent=4)):
+                                match = True
+                        case "text/plain":
+                            if excel_IO.match_prsnlList(prsnlList, text):
+                                match = True
+                except json.JSONDecodeError as e:
+                    print(f"JSON Decode Error: {e}")
+                    print("Invalid JSON:", text)
+#-------------------------------------------------------------------------------------------------------------------
+        global count
+        if match == True:
+            count+=1
+            print(f"\033[96m" + str({count}) + "\033[0m")
+            
+            print()
+            if method == 'GET':
+                print("\033[95m" + "GET" + "\033[0m")
+            
+            print(method, host)
+            print(f"path: {request.path}")
+            
+            if len(queries) > 0:
+                print("queries: [")
+                for k, v in queries:
+                    print(f"\t{k}={v}")
+                print("]")
+            else:
+                print("queries: []")
 
-        print("\n!===============================!\n")
+            print("\n----------------------\n")
 
+
+            if len(headers) > 0:
+                print("headers: [")
+                for k, v in headers:
+                    print(f"\t{k}: {v}")
+                print("]")
+
+            else:
+                print("headers: []")
+
+            print("\n----------------------\n")
+
+            if method == "POST":
+                process_post_request(request, contentType, contentLength)
+            else:
+                print("NONE")
+                        
+            # 매칭된 데이터를 저장할 리스트
+            data_to_write = []
+
+            # 메소드와 호스트 추가
+            data_to_write.append(method)
+            data_to_write.append(host)
+
+            data_to_write.append(f"path: {request.path}")
+
+            # 쿼리 문자열 추가
+            queries = request.query.items()
+            if len(queries) > 0:
+                data_to_write.append("[")
+                for k, v in queries:
+                    data_to_write.append(f"{k}={v}")
+                data_to_write.append("]")
+
+            else:
+                data_to_write.append("queries: []")
+
+            # 헤더 추가
+            headers = request.headers.items()
+            if len(headers) > 0:
+                data_to_write.append("[")
+                for k, v in headers:
+                    data_to_write.append(f"{k}: {v}")
+                data_to_write.append("]")
+
+            else:
+                data_to_write.append("headers: []")
+
+            if method == "POST":
+                process_post_request_excel(request, contentType, contentLength, data_to_write)
+            else:
+                data_to_write.append("headers: []")
+                        
+
+            # 데이터를 엑셀에 기록
+            excel_IO.write_to_excel(host, data_to_write)
 
 
 def process_post_request(request, contentType, contentLength):
     if contentLength > 0:
-        print(f"=> {contentType}: [")
-        text = request.get_text(False)
-        match contentType:
-            case "application/x-www-form-urlencoded":
-                data = parse_qs(text)
-                for k, v in data.items():
-                    print(f"\t{k}={v}")
-            case "application/json":
-                data = json.loads(text)
-                print(json.dumps(data, indent=4))
-            case "text/plain":
-                print(text)
-            case _:
-                print("\t** SKIP **")
-        print("]")
-        # Initialize colorama
+        try:
+            print(f"=> {contentType}: [")
+            text = request.get_text(False)
+            match contentType:
+                case "application/x-www-form-urlencoded":
+                    data = parse_qs(text)
+                    for k, v in data.items():
+                        print(f"\t{k}={v}")
+                case "application/json":
+                    data = json.loads(text)
+                    print(json.dumps(data, indent=4))
+                case "text/plain":
+                    print(text)
+                case _:
+                    print("\t** SKIP **")
+            print("]")
+        except json.JSONDecodeError as e:
+            print("\033[95m" + "json error" + "\033[0m")
 
-        # Print text in blue
+            print(f"JSON Decode Error: {e}")
+            print("Invalid JSON:", text)                     
+
         print("\033[93m" + "어떤 내용이 있다" + "\033[0m")
+        return True
     else:
         print("\033[93m" + "POST이긴 하다" + "\033[0m")
 
-def process_flows(logfile_name, mode):
+def process_post_request_excel(request, contentType, contentLength, data_to_write):
+    if contentLength > 0:
+        try:
+            # 엑셀에서 수식 오류를 방지하기 위해 특수 문자를 이스케이프 처리
+            def escape_excel_formula(value):
+                if isinstance(value, str):
+                    # 문자열이 수식으로 인식되지 않게 앞에 '를 추가
+                    if value.startswith(('=', '+', '-', '@')):
+                        return f"'{value}"
+                    else:
+                        return value
+                elif isinstance(value, list):
+                    # 리스트인 경우 각각의 요소를 이스케이프 처리
+                    return [escape_excel_formula(v) for v in value]
+                return value
 
-    
+            data_to_write.append(f"> {contentType}: [")
+
+            text = request.get_text(False)
+            match contentType:
+                case "application/x-www-form-urlencoded":
+                    data = parse_qs(text)
+                    for k, v in data.items():
+                        escaped_key = escape_excel_formula(k)
+                        escaped_value = escape_excel_formula(v)
+                        data_to_write.append(f"\t{escaped_key}={escaped_value}")
+
+                case "application/json":
+                    data = json.loads(text)
+                    if isinstance(data, dict):
+                        for k, v in data.items():
+                            escaped_key = escape_excel_formula(k)
+                            escaped_value = escape_excel_formula(v)
+                            data_to_write.append(f"{escaped_key}: {escaped_value}")
+                    elif isinstance(data, list):
+                        for i, item in enumerate(data):
+                            escaped_item = escape_excel_formula(item)
+                            data_to_write.append(f"{i}: {escaped_item}")
+
+                case "text/plain":
+                    escaped_text = escape_excel_formula(text)
+                    data_to_write.append(escaped_text)
+
+                case _:
+                    data_to_write.append("\t** SKIP **")
+
+            data_to_write.append("]")  # 데이터의 끝에 닫는 대괄호 추가
+
+        except json.JSONDecodeError as e:
+            print(f"JSON Decode Error: {e}")
+            print("Invalid JSON:", text)
+    else:
+        data_to_write.append("NONE")
+
+
+def process_flows(logfile_name, mode):   
+    package_name = os.path.basename(logfile_name)
+
     try:
         with open(logfile_name, "rb") as logfile:
             freader = io.FlowReader(logfile)
@@ -188,10 +343,22 @@ def process_flows(logfile_name, mode):
                     print("")
 
             elif mode == '2':
+                totcount = 0
                 for f in freader.stream():
-                    prsnlList = excel_IO.excel_prsnlList_input()
-                    process_request_personInfo(f, prsnlList)
+                    global match
+                    
+                    prsnlList_path = input("Enter the path for personal list Excel file: ")
+
+                    prsnlList = excel_IO.excel_prsnlList_input(prsnlList_path)
+                    process_request_personInfo(f, prsnlList, package_name)
                     print("")
+                    match = False
+                    totcount += 1
+                    print(f"\033[94m" + "total count" + str({totcount}) + "\033[0m")
+
+                    print("\n!===============================!\n")
+
+                    
                 # elif mode == 3:
                 #     process_request(f)
                 #     print("")    
